73f66bf7dd63045f15938b21a7541bb1226fae4a
mary-fiorello96 <mariadomenicafiorello@abacatania.it>
2024-06-15 17:23:13 +0200

    html, js, img

---
+++ anno_copyright.js
@@ -0,0 +1,4 @@
+const copyright = document.querySelector('.copyright');
+var data = new Date();
+const anno = data.getFullYear();
+copyright.textContent = "© Copyright " + anno + " by Maria Fiorello";
